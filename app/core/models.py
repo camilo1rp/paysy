d557fb46821853f4bddeb3be4fc2dbe7781ae061
camilo1rp <camilo1rp@gmail.com>
2020-11-07 14:51:35 -0500

    added: models for payment gateway added: models for Zona Pagos

--- app/core/models.py
+++ app/core/models.py
@@ -14,7 +14,7 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """Custom user model that support using email instead of userame"""
+    """Custom user model that support using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     domain = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -22,3 +22,57 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
     USERNAME_FIELD = 'email'
+
+
+class PayGateWay(models.Model):
+    """Payment gateway model that contains basic information"""
+    site = models.ManyToManyField('User', related_name='payways')
+    name = models.CharField(max_length=255)
+    prefix = models.CharField(max_length=4)
+    is_active = models.BooleanField(default=True)
+
+
+class ZonaPagos(models.Model):
+    """Zona Pagos gateway setup"""
+    gateway = models.ForeignKey('PayGateWay',
+                                on_delete=models.CASCADE,
+                                related_name='zona_pagos'
+                                )
+    configuration = models.ForeignKey('ZonaPagosConfig',
+                                      on_delete=models.SET_NULL,
+                                      null=True,
+                                      related_name='zona_pagos'
+                                      )
+    parameters = models.ManyToManyField('ZonaPagosParam',
+                                        through='ZonaPagosParamVal',
+                                        related_name='zona_pagos'
+                                        )
+
+
+class ZonaPagosConfig(models.Model):
+    """Zona Pagos gateway config"""
+    int_id_comercio = models.PositiveIntegerField()
+    str_usuario = models.CharField(max_length=255)
+    str_clave = models.CharField(max_length=255)
+    int_modalidad = models.SmallIntegerField(default=-1)
+    payment_url = models.CharField(max_length=255)
+    consult_url = models.CharField(max_length=255)
+
+
+class ZonaPagosParam(models.Model):
+    """Zona Pagos Parameters"""
+    code = models.SmallIntegerField()
+    description = models.CharField(max_length=511)
+    payment = models.BooleanField(help_text="True: payment parameter , False: configuration parameter",
+                                  default=False)
+
+
+class ZonaPagosParamVal(models.Model):
+    """Zona Pagos Parameters intermediate table with values"""
+    zona_pagos = models.ForeignKey('ZonaPagos',
+                                   on_delete=models.CASCADE
+                                   )
+    zona_pagos_param = models.ForeignKey('ZonaPagosParam',
+                                         on_delete=models.CASCADE
+                                         )
+    value = models.CharField(max_length=255)
