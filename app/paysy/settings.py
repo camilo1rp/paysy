bba582654b1895ee6c78121c2e095e0622ae5571
camilo1rp <camilo1rp@gmail.com>
2020-11-28 16:00:01 -0500

    change nginx configuration

--- app/paysy/settings.py
+++ app/paysy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY',
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['app', 'app:8080']
 
 # Application definition
 
@@ -116,8 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Custom User model
 
 AUTH_USER_MODEL = 'core.User'
