9dd801f81ea06644464b5f769a8e1a553b44b6e0
camilo1rp <camilo1rp@gmail.com>
2020-11-29 11:30:38 -0500

    added: admin view for all models

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,12 @@
-# from django.contrib import admin
+from django.contrib import admin
 
 # Register your models here.
+from core import models
+
+admin.site.register(models.Customer)
+admin.site.register(models.Transaction)
+admin.site.register(models.PayGateWay)
+admin.site.register(models.ZonaPagos)
+admin.site.register(models.ZonaPagosParam)
+admin.site.register(models.ZonaPagosConfig)
+admin.site.register(models.ZonaPagosParamVal)
